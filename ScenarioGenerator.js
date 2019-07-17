c8314b74e0b2b8ddc373cfaa9c5f71b824a0094f
Saul <sawltehhuman@gmail.com>
2019-07-16 17:21:00 -0700

    First lines

---
+++ ScenarioGenerator.js
@@ -0,0 +1,10 @@
+// day array
+var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+// item array
+var items = ['pizza', 'frisbee', 'chocolate', 'candy'];
+// random number 1 to 10
+var dollars = Math.floor(Math.random(10) * 10) + 1;
+var quartervalue = (Math.floor(Math.random(10) * 10) + 1) * 0.25;
+var quarterammount = quartervalue / 0.25;
+console.log(quarterammount);
+console.log(quartervalue);
