1786d0399e4c40ef4528ad709ef9b07ab336eeeb
Saul <sawltehhuman@gmail.com>
2019-07-22 13:40:11 -0700

    Password Wallet and assets (incomplete)

---
+++ PasswordBank.py
@@ -0,0 +1,84 @@
+import hashlib
+import base64
+from appjar import gui
+import os
+import time
+from GenerateMaster import generate_master
+import VerifyMaster
+import AESEncryption
+
+def go(password):
+
+    print('Good to go!')
+
+    win = gui()
+
+
+    def deleteLine(name):
+        fn = 'Passwords.txt'
+        f = open(fn)
+        output = []
+        str=name
+        for line in f:
+            if not line.startswith(str):
+                output.append(line)
+        f.close()
+        f = open(fn, 'w')
+        f.writelines(output)
+        f.close()
+
+    def buttons(b1):
+        print(b1)
+        if b1 == 'Exit':
+            win.stop()
+        if b1 == 'Create':
+            f = open('Passwords.txt', 'a')
+            content = win.getEntry('Password')
+            encrypted_pass = AESEncryption.encrypt2(content)
+            f.write(win.getEntry('Name') + '\t' + win.getEntry('Username') + '\t' + str(encrypted_pass) + '\n')
+            #win.clearAllEntries()
+        if b1 == 'Delete':
+            name_to_delete = win.getEntry('Name')
+            deleteLine(name_to_delete)
+            #win.clearAllEntries()
+        if b1 == 'Read':
+            AESEncryption.decrypt2(win.getEntry('Name'))
+            
+    def start_gui():
+        #time.sleep(10)
+
+        
+        win.setBg('Light blue')
+        win.setFg('Black')
+        win.addLabel('PyWallet')
+        win.addLabelEntry('Name', 2)
+        win.addLabelEntry('Username', 3)
+        win.addLabelEntry('Password', 4)
+        win.addLabel('', 'Password Wallet', 1, 0, 0)
+        win.addLabel('blankspace1', '', 5, 0, 0)
+        win.addButtons(['Create', 'Delete', 'Edit', 'Read', 'Exit'], buttons, 6, 0, 0)
+
+
+        win.go()
+
+
+    start_gui()
+app = gui()
+
+def press():
+    password = app.getEntry('Password')
+    result = VerifyMaster.verify_master(password)
+    if result == True:
+        app.stop()
+        go(password)
+    else:
+        time.sleep(2)
+        app.setLabel('Result', 'Incorrect password')
+app.addLabel('Login')
+app.setBg('Light blue')
+app.addLabel('Result', '')
+app.addSecretLabelEntry('Password')
+app.addButton('Submit', press)
+app.go()
+
+
